20ab9a1b2f79e266e80342ee39ef8ea826df082b
Shoheb Mohammad Tanin <100529705+ShohebTanin@users.noreply.github.com>
2022-02-28 01:37:22 +0600

    Update Config.php

--- login/include/Config.php
+++ login/include/Config.php
@@ -5,7 +5,7 @@
  * Database config variables
  */
 define("DB_HOST", "localhost");
-define("DB_USER", "seexadvo_ilabsbd");
-define("DB_PASSWORD", "NGHy8?Im7f$3");
-define("DB_DATABASE", "seexadvo_ilabsbd");
+define("DB_USER", "");
+define("DB_PASSWORD", "**");
+define("DB_DATABASE", "**");
 ?>
